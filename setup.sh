53acedf670daf4a4c31577aa8785a4ea761640ca
Garret Ranck <garanck2012@yahoo.com>
2022-03-30 15:57:04 -0700

    Adding git clone of tmux logging tool

--- setup.sh
+++ setup.sh
@@ -5,10 +5,11 @@ echo -e "\n\n*** 1. Updating ***\n\n"
 sudo apt update && sudo apt full-upgrade -y
 
 echo -e "\n\n*** 2. Installing packages ***\n\n"
-sudo apt install nano tmux docker.io docker-compose htop net-tools -y
+sudo apt install nano tmux docker.io docker-compose htop net-tools git -y
 
-echo -e "\n\n*** 3.Copying tmux conf ***\n\n"
+echo -e "\n\n*** 3. Setting up tmux ***\n\n"
+git clone https://github.com/tmux-plugins/tmux-logging.git ~/git/
 cp tmux.conf ~/.tmux.conf
 
 echo -e "\n\n*** 4. Launching tmux session ***\n\n"
-tmux new -s dev 
+tmux new -s dev
